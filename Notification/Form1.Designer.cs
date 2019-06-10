ba947323ac1eed532502f8302b4138ac16438964
benjaminRofB <51160128+benjaminRofB@users.noreply.github.com>
2019-06-11 00:41:47 +0300

    Add all Files
    Selenium

---
+++ Notification/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Notification
+{
+    partial class Kufar_Note
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(445, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Parser";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 60);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(100);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(1046, 575);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Kufar_Note
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1070, 647);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Kufar_Note";
+            this.ShowIcon = false;
+            this.Text = "Kufar_Note";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Notification/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace Notification
+{
+    public partial class Kufar_Note : Form
+    {
+        IWebDriver Browser;
+
+
+        public Kufar_Note()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+            Browser.Manage().Window.Maximize();
+            Browser.Navigate().GoToUrl("https://re.kufar.by/");
+
+            
+            //while(true)
+            //{ 
+            WebDriverWait ww = new WebDriverWait(Browser, TimeSpan.FromSeconds(10));
+
+            
+
+            IWebElement SearchCity = Browser.FindElement(By.CssSelector("[value]"));
+            SearchCity.SendKeys("Минск");
+
+            //System.Threading.Thread.Sleep(5000);
+
+            IWebElement element = ww.Until(ExpectedConditions.ElementIsVisible(By.Id("react-autowhatever-1--item-1")));
+            element.Click();
+
+            IWebElement price = ww.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("[data-name='price_filter']")));
+            price.Click();
+
+            IWebElement max_price = Browser.FindElement(By.Id("prc_max"));
+            max_price.SendKeys("86000");
+
+
+            IWebElement search_select = Browser.FindElement(By.CssSelector("[data-name='button_search']"));
+            search_select.Click();
+
+            List<IWebElement> Flat = Browser.FindElements(By.CssSelector("#content> div._3OvBa._2SxM3 > div.t1n7P > div.atMFB._3OvBa._17d3o._2wUH0")).ToList();
+
+            for (int i=0; i<Flat.Count;i++)
+            {
+                
+                    textBox1.AppendText(Flat[i].Text + '\r' + '\n');
+               
+            }
+                //System.Threading.Thread.Sleep(10000);
+                //Browser.Navigate().GoToUrl("https://re.kufar.by/");
+                
+            }
+
+
+        //}
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
